b2760c75638ef209d9c93270a4c574bf14b4fa62
benevolent-trace-0172 <davidmlee4@gmail.com>
2020-01-21 04:54:35 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -126,5 +126,7 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+		 dennis_ritchies_language = ["C", "Assembly"]
+		 programmer_hash[:dennis_ritchie][:languages] = dennis_ritchies_language
+		 programmer_hash
 end
